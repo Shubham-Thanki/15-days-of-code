0e6cb238c395280001a43462942ce487e35b52e9
Shubham Thanki <shubhamthanky@gmail.com>
2022-10-15 12:37:32 +0530

    15/10

--- Day8/sumOfBeauty.py
+++ Day8/sumOfBeauty.py
@@ -2,19 +2,25 @@
 # nums = list(map(int, input().split()))
 nums = [2, 4, 6, 4]
 # nums = [1, 2, 3]
+# nums = [3, 2, 1]
 res = []
 for i in range(1, len(nums)-1):
     preflag = True
     for j in range(0, i):
-        for k in range(i, len(nums)):
-            if (nums[j] < nums[i] < nums[k]):
-                preflag = False
-                res.append(2)
+        if nums[j] >= nums[i]:
+            preflag = False
+    for k in range(i+1, len(nums)):
+        if nums[k] <= nums[i]:
+            preflag = False
+    if (preflag):
+        res.append(2)
+        preflag = False
+    else:
+        preflag = True
 
     if (nums[i - 1] < nums[i] < nums[i + 1]) and preflag:
-        preflag = False
+        preflag = True
         res.append(1)
 
-    if (preflag):
-        res.append(0)
-print(res)
+# print(res)
+print(sum(res))
